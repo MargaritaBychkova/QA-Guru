c12a0ffbea499ebf66fb0579301ea4921fa097cf
Margo <PANTARA_95@mail.ru>
2022-03-31 15:43:38 +0300

    Changes in 12 line for conflict specially

--- src/com/somesite/Main.java
+++ src/com/somesite/Main.java
@@ -9,6 +9,6 @@ public class Main {
     }
     
     public static void another () {
-         System.out.println("ALTERNATIVE METHOD");
+         System.out.println("This line is for conflict");
     }
 }
