664926b6a40921048f20542ce00998c7764a3540
Margo <PANTARA_95@mail.ru>
2022-03-31 15:50:07 +0300

    Changes in 12 line for conflict specially 2.0

--- src/com/somesite/Main.java
+++ src/com/somesite/Main.java
@@ -9,6 +9,6 @@ public class Main {
     }
     
     public static void another () {
-         System.out.println("This line is for conflict!");
+         System.out.println("THIS LINE IS FOR CONFLICT!");
     }
 }
