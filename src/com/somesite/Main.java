f552b28b6ef6a80a179575789be3b928f20bffb9
Margo <PANTARA_95@mail.ru>
2022-03-31 14:24:05 +0300

    Hometask changed to HT

--- src/com/somesite/Main.java
+++ src/com/somesite/Main.java
@@ -3,6 +3,6 @@ package com.somesite;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("First QA hometask");
+        System.out.println("First QA HT");
     }
 }
