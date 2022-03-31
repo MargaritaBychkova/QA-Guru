8f1311b16876459039679d98bfae8acf344d821e
Margo <PANTARA_95@mail.ru>
2022-03-31 15:59:28 +0300

    Added one message line 12

--- src/com/somesite/Main.java
+++ src/com/somesite/Main.java
@@ -9,6 +9,6 @@ public class Main {
     }
     
     public static void another () {
-         System.out.println("THIS LINE IS FOR CONFLICT!");
+         System.out.println("One message");
     }
 }
