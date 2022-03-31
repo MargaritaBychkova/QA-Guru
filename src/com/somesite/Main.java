4fa4283b03183fd10f723b707576c5837736b620
Margo <PANTARA_95@mail.ru>
2022-03-31 14:57:49 +0300

    Added line "Slow branch"

--- src/com/somesite/Main.java
+++ src/com/somesite/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("First QA HT");
         System.out.println("Another line");
+        System.out.println("Slow branch");
     }
 }
