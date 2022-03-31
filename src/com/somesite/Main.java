cd91f5cd7d85bcfcf5b003612b71d2bfb1d70314
Margarita <102693005+MargaritaBychkova@users.noreply.github.com>
2022-03-31 14:56:50 +0300

    Update Main.java

--- src/com/somesite/Main.java
+++ src/com/somesite/Main.java
@@ -6,4 +6,8 @@ public class Main {
         System.out.println("First QA HT");
         System.out.println("Another line");
     }
+    
+    public static void another () {
+         System.out.println("Another method");
+    }
 }
